46878e5bdcea4923f5926c3698ca285e4a30e67e
Monash Chhetri <monash.chhetri22@gmail.com>
2023-05-11 22:45:51 +0530

    added logic to compare the password

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,6 +1,7 @@
 const UserRepository = require('../repository/user-repository');
 const {JWT_KEY} = require('../config/serverConfig');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 class UserService {
     
     constructor(){
@@ -33,6 +34,15 @@ class UserService {
             return response;
         }catch(error){
             console.log("Something went wrong in Verifying Token",error);
+            throw error;
+        }
+    }
+
+    checkPassword(userInputPlainPassword,encryptedPassword){
+        try{
+            return bcrypt.compareSync(userInputPlainPassword,encryptedPassword);
+        }catch(error){
+            console.log("Something went wrong in password comparision");
             throw error;
         }
     }
